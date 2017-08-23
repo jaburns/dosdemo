ec2aa17d3fa9acea6ee9bfbab9bb7ac7f706639b
Jeremy Burns <j@jaburns.net>
2017-08-23 13:16:36 -0600

    Expanded main out in to procedures

--- main.asm
+++ main.asm
@@ -1,31 +1,46 @@
 bits 16
 org 0x100
 
-REFRESH_RATE equ 60
-WAIT_SECS equ 5
+; http://faydoc.tripod.com/cpu/
 
 Start:
-    ; Init video
-        mov ax, 0x13   ; set video mode 13h
-        int 0x10
-        mov ax, 0xA000 ; point ES to video memory
-        mov es, ax
-
-    ; Initialize draw color and frame counter
-        mov ah, 0x40
-        mov bx, REFRESH_RATE * WAIT_SECS
-
+        call Setup
+        xor bl, bl   ; cx -> Sine table lookup index
     .mainLoop:
+        call WaitForRetrace
 
-    ; Draw frame
-        inc ah
-        and ah, 0x4F
-        mov al, ah
+        inc bl
+        mov al, bl
+        call GetCosine
+        shr al, 4
+        add al, 0x40
+        mov ah, al
         xor di, di
         mov cx, 32000
         rep stosw
 
-    ; Wait for next retrace
+        dec word [frameCounter]
+        jnz .mainLoop
+        jmp Exit
+
+
+
+GetSine: ; input al -> output al; clobbers ah
+        mov ah, 0
+        mov si, ax
+        mov al, [sineTable + si]
+        ret
+
+GetCosine: ; same properties as GetSine
+        mov ah, 0
+        add al, 128
+        mov si, ax
+        mov al, [sineTable + si]
+        ret
+
+
+
+WaitForRetrace:       ; clobbers al, dx
         mov dx, 0x03DA
     .waitRetrace:
         in al, dx     ; read from status port
@@ -35,15 +50,25 @@ Start:
         in al, dx
         test al, 0x08
         jz .endRefresh
+        ret
 
-    ; End of main loop
-        dec bx
-        jnz .mainLoop
 
-    ; Restore state and exit
-        mov ax, 0x03  ; return to text mode 0x03
+
+Setup:
+        mov ax, 0x13   ; set video mode 13h
         int 0x10
-        mov ax, 0x4C00  ; exit with code 0
+        mov ax, 0xA000 ; point ES to video memory
+        mov es, ax
+        ret
+
+Exit:
+        mov ax, 0x03   ; return to text mode 0x03
+        int 0x10
+        mov ax, 0x4C00 ; exit with code 0
         int 0x21
+
+
+
+frameCounter: dw 60 * 5
 
 sineTable: incbin "sine.dat"
