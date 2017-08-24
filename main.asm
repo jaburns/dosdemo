b7370d49d9b9347c4e6d9c123a8bcfe11bbf021e
Jeremy Burns <j@jaburns.net>
2017-08-23 22:53:29 -0600

    Fixed column for angles above 128

--- main.asm
+++ main.asm
@@ -6,7 +6,6 @@ org 0x100
         int 0x10
         mov ax, 0xA000 ; point ES to video memory
         mov es, ax
-        xor bx, bx
 
         call MainLoop
 
@@ -29,7 +28,7 @@ MainLoop:
         xor dx, dx  ; dx: video memory offset of current row
         xor di, di  ; di: video memory offset, but incremented by called routines
 
-    .rowsLoop:
+        .rowsLoop:
             call DrawStrip
 
             add dx, 320
@@ -50,54 +49,68 @@ MainLoop:
 ; DI <- vram offset after drawing
 DrawStrip:
         push ax
-        push bx
         push cx
         push dx
+        push bx
 
-    ; get length of first face of column in to bh
+        ; get length of first face of column in to bh
         mov al, bl
         call GetSine
         shr al, 2
         mov bh, al
 
-    ; get length of second face of column in to bl
+        ; get length of second face of column in to bl
         mov al, bl
         add al, 128
         call GetSine
         shr al, 2
         mov bl, al
 
-    ; get length of left empty region in to dh
+        ; get length of left empty region in to dh
         mov dl, bh
         add dl, bl
         shr dl, 1
         mov dh, 120
         sub dh, dl
 
-    ; draw first empty region
+        ; draw first empty region
         xor cx, cx
         mov cl, dh
         mov al, 0
         rep stosb
 
-    ; draw first face
-        mov cl, bh
-        mov al, 0x9
-        rep stosb
+        ; draw two faces, order depending on input angle
+        pop dx
+        cmp dl, 128
+        push dx
+        ja .drawOrderElse
+            mov cl, bh
+            mov al, 0x9
+            rep stosb
+            mov cl, bl
+            mov al, 0xA
+            rep stosb
 
-    ; draw second face
-        mov cl, bl
-        mov al, 0xA
-        rep stosb
+            jmp .drawOrderEnd
 
-    ; draw more empty space to the right
+        .drawOrderElse:
+            mov cl, bl
+            mov al, 0xA
+            rep stosb
+            mov cl, bh
+            mov al, 0x9
+            rep stosb
+
+        .drawOrderEnd:
+
+        ; draw more empty space to the right
         mov cl, 30
         mov ax, 0
         rep stosw
 
+        pop bx
         pop dx
         pop cx
-        pop bx
         pop ax
         ret
 
